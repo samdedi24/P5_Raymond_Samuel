e706f3b019a3ee75d9faa6b0b1ad7f5f87612b30
Samuel Raymond <samraymond24@icloud.com>
2020-08-05 14:20:42 +0200

    Big update
    Probleme suppresion produi mis a jour + refonte à partir des inpus côté JS

--- js/cart.js
+++ js/cart.js
@@ -51,7 +51,7 @@ function summary () {
         let rowProduct = document.createElement("tr");
         let nameProduct = document.createElement("td");
         let productPriceUnit = document.createElement("td");
-        let removeProduct = document.createElement("button");
+        let removeProduct = document.createElement("i");
 
         //Annuler le produit selectionner sur l'icone
         rowTable.setAttribute("id", "product"+ i);
@@ -69,7 +69,6 @@ function summary () {
         //Row content
         nameProduct.innerHTML = product.name;
         productPriceUnit.textContent = product.price / 100 + " €";
-        //columnUnity.textContent = userCart;
         });
 
         //Ligne total de tout 
@@ -95,77 +94,142 @@ function summary () {
 
 function cancelProduct (i) {
     console.log("Retire le produit " + i);
-    //userCart.splice(i, 1);
+    userCart.splice(i, 1);
+    console.log("Administration : " + userCart);
+    localStorage.clear();
+    localStorage.setItem('userCart', JSON.stringify(userCart));
+    window.location.reload();
 };
 
-class Client {
-    constructor(forNom, forPrenom, forMail, forAdresse, forVille) {
-        (this.forNom = forNom),
-        (this.forPrenom = forPrenom),
-        (this.forMail = forMail),
-        (this.forAdresse = forAdresse),
-        (this.forVille = forVille);
+//Vérification des inputs
+
+function verifInput () {
+    let forNom = document.getElementById("forNom").value;
+    let forPrenom = document.getElementById("forPrenom").value;
+    let forMail = document.getElementById("forMail").value;
+    let forAdresse = document.getElementById("forAdresse").value;
+    let forVille = document.getElementById("forVille").value;
+
+    //utilisez [A-Za-z] pour minuscules et majuscules et [A-Z] pour les majuscules uniquement
+    let verifString = /[a-zA-Z]/;
+    //correspond à tout nombre compris entre 0 et 9
+    let verifNumber = /[0-9]/;
+    //verif email et spécial caract
+    let verifMail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let verifSpeChara = /[§!@#$%^&*(),.?":{}|<>]/;
+    //fin de controle
+    let verifMessage = "";
+    let contact;
+
+    //Verif des différents inputs 2
+    if (verifNumber.test(forNom) == true || verifSpeChara.test(forNom) == true || forNom == ""){
+        verifMessage = "Vérifier votre nom";
+    } else {
+        console.log("Le nom est ok");
+    };
+
+    if (verifNumber.test(forPrenom) == true || verifSpeChara.test(forPrenom) == true || forPrenom == ""){
+        verifMessage = verifMessage + "\n" + "Vérifier votre prénom";
+    } else {
+        console.log("Le prénom et ok");
+    };
+
+    if(verifMail.test(forMail) == false){
+        verifMessage = verifMessage + "\n" + "Vérifier votre email";
+    } else {
+        console.log("Adresse email ok");
+    };
+    
+    if(verifSpeChara.test(forAdresse) == true || forAdresse == ""){
+        verifMessage = verifMessage + "\n" + "Vérifier votre adresse";
+    } else {
+        console.log("l'adresse est ok");
+    };
+    
+    if(verifSpeChara.test(forVille) == true && verifNumber.test(forVille) == true || forVille == ""){
+        verifMessage = verifMessage + "\n" + "Vérifier votre ville"
+    } else {
+        console.log("La ville est ok")
+    };
+
+    //Input pas bon message alertant
+    if (verifMessage != "") {
+        alert("Il est nécessaire de :" + "\n" + verifMessage);
+    } else {
+        //objet contact controllers ligne 39
+        contact = {
+            firstName : forNom,
+            lastName : forPrenom,
+            address : forAdresse,
+            city : forVille,
+            email : forMail
+        };
+        return contact;
+    };
+};
+
+//fonction on vérifie si l'utilisateur à bien un artcile au panier pour pourvoir continuer
+
+function verifCart () {
+    let statutCart = JSON.parse(localStorage.getItem("userCart"));
+
+    if (statutCart == null) {
+        alert("Il y a eu un problème, recharger votre page");
+        return false
+    } else if (statutCart.length < 1 || statutCart == null) {
+        alert("Panier vide");
+        return false;
+    } else {
+        JSON.parse(localStorage.getItem("userCart")).forEach((product) =>{
+            products.push(product._id);
+        });
+        return true;
     }
-}
+};
 
-//Vérification formulaire
+//Post de l'API
+function sendData (objectRequest) {
+    return new Promise((resolve) =>{
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                sessionStorage.setItem("order", this.responseText);
+                document.forms["form-cart"].action = './order-confirm.html';
+                document.forms["form-cart"].submit();
+                resolve(JSON.parse(this.responseText));
+            }
+        };
+        request.open("POST", API + "order");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(objectRequest);
+    });
+};
 
-let forNom = document.getElementById("forNom");
-let forPrenom = document.getElementById("forPrenom");
-let forMail = document.getElementById("forMail");
-let forAdresse = document.getElementById("forAdresse");
-let forVille = document.getElementById("forVille");
-let orderButton = document.querySelector(".order-submit");
-let validationButton = document.querySelector(".btn-primary");
+//Validation infos et panier
 
-orderButton.addEventListener("click", function (event){
-    event.preventDefault();
-    let newClient = new Client(
-        forNom.value,
-        forPrenom.value,
-        forMail.value,
-        forAdresse.value,
-        forVille.value
-    );
+function validCart () {
+    let btnPrimary = document.getElementById("passedOrder");
+    btnPrimary.addEventListener("click", function(){
+        if (verifCart() == true && verifInput() != null){
+            console.log("Envoi possible");
+            let object = {
+                contact,
+                products
+            };
+            let objectRequest = JSON.stringify(object);
+            sendData(objectRequest);
 
-    //POST api
-    fetch("http://localhost:3000/api/cameras/order", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            contact : {
-                firstName: newClient.forNom,
-                lastName: newClient.forPrenom,
-                address: newClient.forAdresse,
-                city: newClient.forVille,
-                email: newClient.forMail,
-            },
-        }),
-    })
-    .then ((res) => {
-        if (res.ok) {
-            alert("Vos indormations sont enregistrées");
-            validationButton.classList.remove("disabled");
-            return res.json();
+            //Après commande effectuée
+            contact = {};
+            products = [];
+            localStorage.clear();
         } else {
-            alert("Vous devez remplir tout les champs")
-        }
-    })
-    .then ((data) =>{
-        localStorage.setItem("orderInfos", JSON.stringify(data));
-    }) 
-    .catch((error) => console.log("erreur de type : ", error));
-});
-
-
-
-
-
-
-
-
-
+            console.log("Erreur comme d'hab");
+        };
+    });
+};
 
 
 summary();
-cancelProduct();
+//cancelProduct();
+verifCart();
